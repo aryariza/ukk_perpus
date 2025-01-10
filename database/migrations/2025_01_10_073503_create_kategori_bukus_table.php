7ac94f12755ad6d81e9d731437817b4a4a7d2d93
Air <aryarizapratama25@gmail.com>
2025-01-10 14:56:50 +0700

    commit ke dua membuat dan mengubah migration dan models

--- database/migrations/2025_01_10_073503_create_kategori_bukus_table.php
+++ database/migrations/2025_01_10_073503_create_kategori_bukus_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('kategori_bukus', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_kategori');
+            $table->string('kategori_buku');
             $table->timestamps();
         });
     }
