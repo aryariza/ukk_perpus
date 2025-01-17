b8e999444becade34832bbf4f1b1e9e3d0f39cd6
Air <aryarizapratama25@gmail.com>
2025-01-17 14:19:40 +0700

    end db clear

--- app/Models/Pinjam.php
+++ app/Models/Pinjam.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use\App\Models\Buku;
+use\App\Models\User;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pinjam extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongTo(User::class);
+    }
+    public function buku()
+    {
+        return $this->belongTo(Buku::class);
+    }
 }
--- app/Models/ulasan.php
+++ app/Models/ulasan.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Buku;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ulasan extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongTo(User::class);
+    }
+    public function buku()
+    {
+        return $this->belongTo(Buku::class);
+    }
 }
